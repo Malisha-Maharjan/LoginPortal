66d1eb939af3f5a8aa0c3cbb6810f5511a2e19d3
Malisha Maharjan <malishamaharjan00@gmail.com>
2022-10-19 07:09:00 +0545

    inital commit

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-import re
-import sys
-
 import mysql.connector
+import sys
+import re
+import os
+from flask_wtf.file import FileField, FileAllowed
+from flask_wtf import FlaskForm
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+UPLOAD_FOLDER = os.path.join('static', 'images')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 db = mysql.connector.connect(
     host="localhost",
@@ -17,12 +22,13 @@ cursor = db.cursor(dictionary=True)
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'your secret key'
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('user/index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,16 +48,16 @@ def login():
                 return redirect(url_for('admin'))
             else:
                 msg = 'Incorrect username/password'
-                return render_template('login.html', msg=msg)
+                return render_template('user/login.html', msg=msg)
         except Exception as e:
             msg = e.with_traceback(None)
     else:
         if session.get('username'):
             return redirect(url_for('admin'))
-        return render_template('login.html', msg=msg)
+        return render_template('user/login.html', msg=msg)
 
 
-@app.route('/logout', methods=['POST'])
+@app.route('/logout', methods=['GET'])
 def logout():
     try:
         session.pop('username')
@@ -85,7 +91,7 @@ def register():
         except Exception as e:
             msg = e.with_traceback(None)
 
-    return render_template('register.html', msg=msg)
+    return render_template('user/register.html', msg=msg)
 
 
 @app.route('/admin')
@@ -96,14 +102,14 @@ def admin():
     cursor.execute(
         'SELECT * FROM accounts')
     account = cursor.fetchall()
-    return render_template('admin.html', account=account)
+    return render_template('user/admin.html', account=account)
 
 
 @app.route('/user')
 def user():
     cursor.execute('SELECT * FROM accounts')
     account = cursor.fetchall()
-    return render_template('user.html', account=account)
+    return render_template('user/user.html', account=account)
 
 
 @app.route('/edit/<id>')
@@ -112,7 +118,7 @@ def edit(id):
     cursor.execute(
         'SELECT * FROM accounts WHERE id=%s', (id, ))
     account = cursor.fetchone()
-    return render_template('edit.html', account=account)
+    return render_template('user/edit.html', account=account)
 
 
 @app.route('/update', methods=['POST'])
@@ -137,7 +143,7 @@ def update():
         db.commit()
         flash('The record has been successfully updated.')
         return redirect(url_for('user'))
-    return render_template('edit.html', account=account, msg=msg)
+    return render_template('user/edit.html', account=account, msg=msg)
 
 
 @app.route('/delete/<id>')
@@ -146,7 +152,7 @@ def delete(id):
     cursor.execute(
         'SELECT * FROM accounts WHERE id=%s', (id, ))
     account = cursor.fetchone()
-    return render_template('delete.html', account=account)
+    return render_template('user/delete.html', account=account)
 
 
 @app.route('/destroy', methods=['POST'])
@@ -170,17 +176,79 @@ def destroy():
         return redirect(url_for('user'))
 
 
-@app.route('/product', methods=['POST', 'GET'])
+@app.route('/product')
 def product():
+    return render_template('product/productpage.html')
+
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
     if request.method == 'POST':
         productname = request.form['productname']
         price = request.form['price']
-        productimage = request.form['productimage']
+        picture = request.files['productimage']
+        picture_filename = secure_filename(picture.filename)
+        picture.save(os.path.join(
+            app.config['UPLOAD_FOLDER'], picture_filename))
+        image_path = os.path.join(
+            app.config['UPLOAD_FOLDER'], picture_filename)
+
         cursor.execute(
-            'INSERT INTO products(productname, price, productimage), VALUES(%s, %s, )')
+            'INSERT INTO products (productname, productimage, price) VALUES(%s, %s, %s)', (productname, image_path, price, ))
+        db.commit()
+        return render_template('product/add.html')
 
-    return render_template('product.html')
+    return render_template('product/add.html')
 
+
+@app.route('/display', methods=['POST', 'GET'])
+def display():
+    cursor.execute('SELECT * FROM products')
+    product = cursor.fetchall()
+    return render_template('product/display.html', product=product)
+
+
+@app.route('/productedit/<id>')
+def productedit(id):
+    cursor.execute('SELECT * FROM products WHERE id=%s', (id, ))
+    product = cursor.fetchone()
+    print(product, file=sys.stdout)
+    return render_template('product/productedit.html', product=product)
+
+@app.route('/productupdate', methods=['POST'])
+def productupdate():
+    productname = request.form['productname']
+    price = request.form['price']
+
+    print(productname, price, file=sys.stdout)
+    # cursor.execute('UPDATE products SET productname=%s, price=%s, pictureimage=%s WHERE id=%s',
+    #                 (productname, price, image_path, id, ))
+    # db.commit()
+    return redirect(url_for('display'))
+
+@app.route('/deleteproduct/<id>')
+def deleteproduct(id):
+    # if request.method == 'GET':
+    cursor.execute(
+        'SELECT * FROM products WHERE id=%s', (id, ))
+    product = cursor.fetchone()
+    return render_template('product/deleteproduct.html', product=product)
+
+
+@app.route('/destroyproduct', methods=['POST'])
+def destroyproduct():
+    # alert = ''
+    print(request.method, file=sys.stdout)
+    productname = request.form['productname']
+    cursor.execute('SELECT * FROM products WHERE productname=%s', (productname, ))
+    product = cursor.fetchone()
+
+    cursor.execute('DELETE FROM products WHERE productname=%s',
+                    (productname, ))
+    flash("Successfully deleted!!")
+    db.commit()
+
+    return redirect(url_for('display'))
 
 if __name__ == '__main__':
     app.debug = True
